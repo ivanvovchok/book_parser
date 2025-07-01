c065ecd8d0ba333c48c41f2aeccbf41ba8e6b463
Ivan Vovchok <elvovchok@gmail.com>
2025-07-01 19:59:40 +0300

    Add book model class

---
+++ app/Models/Book.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\BookStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title',
+        'isbn',
+        'page_count',
+        'published_date',
+        'thumbnail_url',
+        'short_description',
+        'long_description',
+        'status',
+    ];
+
+    protected $casts = [
+        'published_date' => 'date',
+        'status' => BookStatus::class,
+        'page_count' => 'integer',
+    ];
+
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class, 'authors_books', 'book_id', 'author_id');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'books_categories', 'book_id', 'category_id');
+    }
+}
